19c893281a80b5c0e81d79a99233d68a8a71b69f
xanxys <xanxys@gmail.com>
2018-11-05 21:07:44 +0900

    Techtree depth logic

--- main.py
+++ main.py
@@ -11,7 +11,21 @@ import parser_jp
 plt.rcParams['font.family'] = 'Droid Sans'
 
 
-def visualize_interactive_ugly(table):
+def visualize_print_text(table):
+    for ix_code in range(table.num_codes):
+        curr_code = table.coeff_code_names[ix_code]
+        top_inputs = sorted(
+            enumerate(table.coeff_matrix[:, ix_code]), key=lambda t: t[1], reverse=True)
+
+        print(curr_code, ["%.3f:%s" % (
+            k, table.coeff_code_names[i]) for (i, k) in top_inputs if k > 0.05])
+
+
+def to_dag(table):
+    """
+    Returns nx.DiGraph, with vertex label being codename.
+    The graph is guranteed to be a connected DAG (Directed Acyclic Graph).
+    """
     df_srcs = []
     df_dsts = []
 
@@ -20,9 +34,6 @@ def visualize_interactive_ugly(table):
 
         top_inputs = sorted(
             enumerate(table.coeff_matrix[:, ix_code]), key=lambda t: t[1], reverse=True)
-
-        print(curr_code, ["%.3f:%s" % (
-            k, table.coeff_code_names[i]) for (i, k) in top_inputs if k > 0.05])
 
         for (i, k) in top_inputs:
             if k < 0.05:
@@ -35,31 +46,55 @@ def visualize_interactive_ugly(table):
             df_dsts.append(curr_code)
 
     df = pd.DataFrame({'from': df_srcs, 'to': df_dsts})
-    G = nx.from_pandas_dataframe(
+    graph = nx.from_pandas_dataframe(
         df, 'from', 'to', create_using=nx.DiGraph())
 
+    # Assert DAG property. This is data-dependent, so failure can very well be a data issue rather than a bug.
     try:
-        print("cycles:", nx.find_cycle(G))
+        cycles = nx.find_cycle(graph)
+        assert(False)
     except nx.exception.NetworkXNoCycle:
-        print("--> no cycles!")
-        pass
+        return graph
 
-    pos = nx.spring_layout(G, iterations=500)
 
-    nx.draw(G, pos=pos, with_labels=True, node_size=1500,
+def visualize_interactive_ugly(table):
+    graph = to_dag(table)
+
+    pos = nx.spring_layout(graph, iterations=500)
+    nx.draw(graph, pos=pos, with_labels=True, node_size=1500,
             alpha=0.3, arrows=True, font_family='VL Gothic')
     plt.title("Techtree")
     plt.show()
 
 
 def visualize_civ_style(table):
-    pass
+    graph = to_dag(table)
+
+    # Get leftmost node & rightmost node, and length between them.
+    srcs = [node for node in graph.nodes() if not nx.ancestors(graph, node)]
+    dsts = [node for node in graph.nodes() if not nx.descendants(graph, node)]
+    assert(len(set(srcs) & set(dsts)) == 0)
+    print("S:", srcs)
+    print("D:", dsts)
+
+    s_d_paths = []
+    for s in srcs:
+        for d in dsts:
+            try:
+                s_d_paths.append(((s, d), len(nx.shortest_path(graph, s, d))))
+            except nx.exception.NetworkXNoPath:
+                pass
+    assert(s_d_paths)
+    dist, longest_path = max(s_d_paths, key=lambda x: x[1])
+    print(dist, longest_path)
 
 
 def main():
     table = parser_jp.InputCoefficientTable(
         'data-jp/2011-input_coeff_table.csv')
-    visualize_interactive_ugly(table)
+    visualize_print_text(table)
+    # visualize_interactive_ugly(table)
+    visualize_civ_style(table)
 
 
 if __name__ == '__main__':
