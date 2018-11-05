d898bba70091331220a236078b9e148a0ea847db
xanxys <xanxys@gmail.com>
2018-11-05 21:58:13 +0900

    Super naive segmentation

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def visualize_print_text(table):
             k, table.coeff_code_names[i]) for (i, k) in top_inputs if k > 0.05])
 
 
-def to_dag(table):
+def to_graph(table):
     """
     Returns nx.DiGraph, with vertex label being codename.
     The graph is guranteed to be a connected DAG (Directed Acyclic Graph).
@@ -49,16 +49,17 @@ def to_dag(table):
     graph = nx.from_pandas_dataframe(
         df, 'from', 'to', create_using=nx.DiGraph())
 
-    # Assert DAG property. This is data-dependent, so failure can very well be a data issue rather than a bug.
-    try:
-        cycles = nx.find_cycle(graph)
-        assert(False)
-    except nx.exception.NetworkXNoCycle:
-        return graph
+    # DAG-ness is data-dependent, so failure can very well be a data issue rather than a bug.
+    print(list(nx.simple_cycles(graph)))
+    # TODO: Come up with generic cycle-breaking solution.
+    # This is not just a pure algorithm question, it also touches fundamentals of what we're trying to draw here.
+    graph.remove_edge('電力', '石炭・原油・天然ガス')
+    assert(nx.is_directed_acyclic_graph(graph))
+    return graph
 
 
 def visualize_interactive_ugly(table):
-    graph = to_dag(table)
+    graph = to_graph(table)
 
     pos = nx.spring_layout(graph, iterations=500)
     nx.draw(graph, pos=pos, with_labels=True, node_size=1500,
@@ -67,8 +68,9 @@ def visualize_interactive_ugly(table):
     plt.show()
 
 
+# graph.num_nodes is about 100~200, don't worry about bad time complexity.
 def visualize_civ_style(table):
-    graph = to_dag(table)
+    graph = to_graph(table)  # not necessarily a DAG
 
     # Get leftmost node & rightmost node, and length between them.
     srcs = [node for node in graph.nodes() if not nx.ancestors(graph, node)]
@@ -81,20 +83,44 @@ def visualize_civ_style(table):
     for s in srcs:
         for d in dsts:
             try:
-                s_d_paths.append(((s, d), len(nx.shortest_path(graph, s, d))))
+                path = nx.shortest_path(graph, s, d)
+                s_d_paths.append((path, len(path)))
             except nx.exception.NetworkXNoPath:
                 pass
     assert(s_d_paths)
-    dist, longest_path = max(s_d_paths, key=lambda x: x[1])
-    print(dist, longest_path)
+    longest_path, dist = max(s_d_paths, key=lambda x: x[1])
+    code_to_depth = {}
+    for (depth, code) in enumerate(longest_path):
+        code_to_depth[code] = depth
+
+    # Super naive 0-rounded depth assignment, treating longest_path as
+    # the "trunk" (i.e. each node has a depth constraint).
+    current_depth = 0
+    for n in nx.topological_sort(graph):
+        if n in code_to_depth:
+            current_depth = code_to_depth[n]
+        else:
+            code_to_depth[n] = current_depth
+    # print(graph.number_of_nodes(), len(nx.topological_sort(graph)), len(code_to_depth), table.num_codes)
+    assert(len(code_to_depth) <= table.num_codes)  # note some vertices are omitted in to_graph (TBC)
+    
+    depth_to_nodes = {}
+    for (n, d) in code_to_depth.items():
+        if d not in depth_to_nodes:
+            depth_to_nodes[d] = set()
+        depth_to_nodes[d].add(n)
+
+    for (d, ns) in depth_to_nodes.items():
+        print("depth=%d: %s" % (d, ns))
+    
 
 
 def main():
     table = parser_jp.InputCoefficientTable(
         'data-jp/2011-input_coeff_table.csv')
     visualize_print_text(table)
-    # visualize_interactive_ugly(table)
     visualize_civ_style(table)
+    # visualize_interactive_ugly(table)
 
 
 if __name__ == '__main__':
